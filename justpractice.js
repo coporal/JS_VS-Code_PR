03a5886ccbc47aef1f575089a99028df206d6bc5
DHinSL <12161537@inha.edu>
2021-04-01 13:47:24 +0900

    js day 01 in sl

---
+++ justpractice.js
@@ -0,0 +1,13 @@
+var foo='hello'
+console.log(typeof foo);
+foo=2;
+//같은 변수에 여러타입 값 할당 가능
+console.log(typeof foo);
+
+
+var binary=0b01;
+console.log(binary);
+
+
+var result=2;
+console.log(result);
